902caf8e4ee5c1cb32ea56eb8076e689842f3f6c
Ervis <lervis87@gmail.com>
2024-01-18 17:19:10 +0000

    adds mui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
-
+import { Box, Button } from "@mui/material";
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+    <Box mt={3}>
+      <Button variant="contained">Scemo chi clicca</Button>
+    </Box>
+
     <h1>Ciao MICHI 👋</h1>
     <p>
       <Link href="/about">About</Link>
