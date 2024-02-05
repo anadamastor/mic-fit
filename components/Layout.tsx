32b6f12a1af7179bdeca80a97a62e7b0d3518019
Ervis <lervis87@gmail.com>
2024-02-05 22:16:56 +0000

    increases font sizee

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@mui/material";
+import Head from "next/head";
 import { ReactNode } from "react";
 
 type Props = {
@@ -7,17 +8,31 @@ type Props = {
 };
 
 const Layout = ({ children, title = "Michela Mancini" }: Props) => (
-  <Container
-    sx={{
-      bgcolor: "#4158D0",
-      height: "98vh",
-      paddn: "0px",
-      backgroundImage:
-        " linear-gradient(40deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
-    }}
-  >
-    {children}
-  </Container>
+  <>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Michela Mancini Fitness" />
+      <meta
+        property="og:description"
+        content="Istruttrice Boxe Thailandese Federkombat..."
+      />
+      <meta property="og:image" content="images/pic_michi.jpg" />
+      <meta property="og:image:width" content="600" />
+      <meta property="og:image:height" content="400" />
+      <title>Michela Mancini Fitness</title>
+    </Head>
+    <Container
+      sx={{
+        bgcolor: "#4158D0",
+        pb: "50px",
+        backgroundImage:
+          " linear-gradient(40deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
+      }}
+    >
+      {children}
+    </Container>
+  </>
 );
 
 export default Layout;
